782076131d67c21609e26a36ab2d1e5f90cf7a5c
sps326532 <sps326532@gmail.com>
2019-11-15 10:38:38 +0800

    期中考

--- code/01-http/01-koaServer/koaServer.js
+++ code/01-http/01-koaServer/koaServer.js
@@ -2,9 +2,9 @@ const Koa = require('koa');
 const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
-  console.log('url:', ctx.url)     //會印出網址
+  console.log('url:', ctx.url)     //網址是根目錄
   console.log('  method:', ctx.method)    //傳輸方法是GET
-  console.log('  headers:', ctx.headers)  //表頭
+  console.log('  headers:', ctx.headers)  //表頭，執行後出現的表頭很長
   ctx.type = 'text/html'
   ctx.body = 'Hello World';
 });
@@ -13,3 +13,6 @@ if (!module.parent) {
   app.listen(3000)
   console.log(`Server running at http://localhost:3000/`)
 }
+
+//ctx.url永遠都可以拿它來取網址
+//ctx意思，當有一個瀏覽器連接到你的伺服器的時候，那伺服器會把所有傳來的資訊傳到ctx傳給你，回應給瀏覽器也是放到ctx某些欄位給他就可以了
